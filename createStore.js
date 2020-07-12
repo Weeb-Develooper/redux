10638ca3c326fe860d68d667faedc2e2023bd8c4
Jonathan-Odia Osahon Emmanuel <emmyodia@gmail.com>
2020-07-12 07:03:01 +0100

    initial

---
+++ createStore.js
@@ -0,0 +1,13 @@
+function createStore(){
+    let state;
+    let listeners = [];
+    const getState = () => state;
+
+    const subscribe = (listener) => listeners.push(listener);
+    
+
+    return {
+        getState,
+        subscribe
+    }
+}
