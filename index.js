2e68c90d9660253df8510298586acf5826dd19c8
Jonathan-Odia Osahon Emmanuel <emmyodia@gmail.com>
2020-07-30 16:49:45 +0100

    reducer created

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
+function todos(state=[], action){
+  if(action.type == "ADD_TODO"){
+    return state.concat([action.todo]);
+  }
+
+  return state;
+}
+
 function createStore(){
   const store;
+  const listeners = [];
 
   const getState = () => this.state;
   
+  const subscribe = (listener) => {
+    listeners.push(listener);
+  }
+
+
   return {
-    getState
+    getState,
+    subscribe
   }
 }
+
+const store = createStore();
+store.subscribe(() => console.log("the new state is", store.getState()));
+store.subscribe(() => console.log('the state has changed'));
