edf06bca9a5047bf57149a4552f23e411b8e20cb
Jonathan-Odia Osahon Emmanuel <emmyodia@gmail.com>
2020-07-30 17:42:46 +0100

    adding dispatch function

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
+
+function createStore(reducer){
+  const state;
+  const listeners = [];
+
+  const getState = () => state;
+  
+  const subscribe = (listener) => {
+    listeners.push(listener);
+  }
+
+  const dispatch = (action) => {
+    state = reducer(state,action);
+
+    listeners.forEach((listener) => listener());
+  }
+
+
+  return {
+    getState,
+    subscribe,
+    dispatch
+  }
+}
+
 function todos(state=[], action){
   if(action.type == "ADD_TODO"){
     return state.concat([action.todo]);
@@ -6,23 +31,7 @@ function todos(state=[], action){
   return state;
 }
 
-function createStore(){
-  const store;
-  const listeners = [];
 
-  const getState = () => this.state;
-  
-  const subscribe = (listener) => {
-    listeners.push(listener);
-  }
-
-
-  return {
-    getState,
-    subscribe
-  }
-}
-
-const store = createStore();
+const store = createStore(todos);
 store.subscribe(() => console.log("the new state is", store.getState()));
 store.subscribe(() => console.log('the state has changed'));
