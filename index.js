32889195c5d7d78af7dafb2ba6cb90b99033d4d2
Jonathan-Odia Osahon Emmanuel <emmyodia@gmail.com>
2020-08-02 23:07:29 +0100

    customizing the dispatch

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 function createStore(reducer){
-  const state;
-  const listeners = [];
+  let state;
+  let listeners = [];
 
   const getState = () => state;
   
@@ -23,15 +23,66 @@ function createStore(reducer){
   }
 }
 
-function todos(state=[], action){
-  if(action.type == "ADD_TODO"){
-    return state.concat([action.todo]);
-  }
+const ADD_TODO = "ADD_TODO";
+const REMOVE_TODO = "REMOVE_TODO";
+const TOGGLE_TODO = "TOGGLE_TODO";
+const ADD_GOAL = "ADD_GOAL";
+const REMOVE_GOAL = "REMOVE_GOAL";
 
-  return state;
+function addTodo(todo){
+  return {
+    type:ADD_TODO,
+    todo
+  }
 }
 
+function removeTodo(id){
+  return {
+    type:REMOVE_TODO,
+    id
+  }
+}
 
-const store = createStore(todos);
-store.subscribe(() => console.log("the new state is", store.getState()));
-store.subscribe(() => console.log('the state has changed'));
+function toggleTodo(id){
+  return {
+    type:TOGGLE_TODO,
+    id
+  }
+}
+
+function addGoal(goals){
+  return {
+    type:ADD_GOAL,
+    goal
+  }
+}
+
+function removeGoal(id){
+  return {
+    type:REMOVE_GOAL,
+    id
+  }
+}
+
+function todos(state=[], action){
+  if(action.type == ADD_TODO){
+    return state.concat([action.todo]);
+  }else if(action.type == REMOVE_TODO){
+    return state.filter((todo)=> todo.id !== action.id);
+  }else if(action.type == TOGGLE_TODO){
+    return state.map((todo) => todo.id !== action.id ? todo : Object.assign({},todo,{complete : !todo.complete}));
+  }else{
+    return state;
+  }
+}
+
+function goals(state=[], action){
+  if(action.type == ADD_GOAL){
+    return state.concat([action.goal]);
+  }else if(action.type == REMOVE_GOAL){
+    return state.filter((goal)=> goal.id !== action.id);
+  }else{
+    return state;
+  }
+}
+
